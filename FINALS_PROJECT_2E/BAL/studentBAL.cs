df8b67b81801cdfaf7fd2e43ade5b81f6fffd613
Jomscielle <pamisjj@gmail.com>
2023-11-26 08:45:28 +0800

    adding Bal

---
+++ FINALS_PROJECT_2E/BAL/studentBAL.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FINALS_PROJECT_2E.BAL
+{
+    class studentBAL
+    {
+        public int student_id { get; set; }
+        public string student_firstname { get; set; }
+        public string studet_lastname { get; set; }
+        public DateTime dateTime { get; set; }
+        public string phone { get; set; }
+        public string address { get; set; }
+        public byte[] photo { get; set; }
+    }
+}
